a159461a2c6b348a6c1a72515dc668ca309a00cd
Petri Majamäki <petri.majamaki@layetteapp.com>
2018-08-21 16:29:56 +0300

    Changed macro
    Changed macro for Mirai.

--- keyboards/mirai/keymaps/mira/keymap.c
+++ keyboards/mirai/keymaps/mira/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NUHS,  KC_1,     KC_2,    KC_3,      KC_4,    KC_5,   KC_6,    KC_7,   KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,                      KC_BSPC,    KC_QUOT, \
   KC_TAB,   KC_Q,     KC_W,    KC_E,      KC_R,    KC_T,   KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     MACRO03,                     KC_RBRC,  KC_BSLS,    KC_LBRC, \
   KC_CAPS,  KC_A,     KC_S,    KC_D,      KC_F,    KC_G,   KC_H,    KC_J,   KC_K,     KC_L,     MACRO02,            MACRO01,                     KC_ENT,     KC_PGUP, \
-  KC_LSFT,  KC_Z,              KC_X,      KC_C,    KC_V,   KC_B,    KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSHF,  KC_UP,      KC_PGDOWN, \
+  KC_LSFT,  KC_Z,              KC_X,      KC_C,    KC_V,   KC_B,    KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSHIFT,  KC_UP,      KC_PGDOWN, \
   KC_LCTRL, KC_LGUI,  KC_LALT,                             KC_SPACE,                            KC_RALT,  KC_INS,   KC_LOCKING_SCROLL, KC_LEFT,  KC_DOWN,    KC_RGHT
 
   ) ,
@@ -49,30 +49,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
 KC_SCLN, KC_QUOT
 */
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-      switch(id) {
-        case MACRO01:
-          if(record->event.pressed){
-            SEND_STRING(SS_LSFT("'")"a");
-            return false;
-          }
-        case MACRO02:
-          if(record->event.pressed){
-            SEND_STRING(SS_LSFT("'")"o");
-            return false;
-          }
-        case MACRO03:
-          if(record->event.pressed){
-            SEND_STRING(SS_RALT("a"));
-            return false;
-          }
-        default:
-          return true;
-        }
-    return MACRO_NONE;
-};
-
 
 void matrix_init_user(void) {
 
@@ -83,7 +59,63 @@ void matrix_scan_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
+  uint8_t shifted;
+  uint16_t s_keycode = keycode;
+  bool *k_shifted = false;
+
+  switch (keycode) {
+      case MACRO01:
+        if(record->event.pressed){
+          SEND_STRING(SS_LSFT("'")"a");
+          return false;
+        }
+      case MACRO02:
+        if(record->event.pressed){
+          SEND_STRING(SS_LSFT("'")"o");
+          return false;
+        }
+    default:
+      return true;
+  }
+
+   /*   switch(keycode) {
+      case MACRO01:
+        SEND_STRING(SS_LSFT("'")"a");
+        return false;
+      case MACRO02:
+        SEND_STRING(SS_LSFT("'")"o");
+        return false;
+      }
+*/
+
+ if(record->event.pressed){
+    switch(keycode) {
+      case MACRO01:
+        SEND_STRING(SS_LSFT("'")"a");
+        return false;
+      case MACRO02:
+        SEND_STRING(SS_LSFT("'")"o");
+        return false;
+      }
+  }
+
+  shifted = get_mods() & (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT));
+
+  // Keydown. If shift is currently pressed, register its alternate keycode.
+  if (record->event.pressed && shifted) {
+    *k_shifted = true;
+    register_code(s_keycode);
+    return false;
+    // Keyup. If shift was pressed back when the key was pressed, unregister
+    // its alternate keycode.
+  }else if (!(record->event.pressed) && *k_shifted) {
+    *k_shifted = false;
+    unregister_code(s_keycode);
+    return false;
+    // Otherwise, behave as normal.
+  } else {
+    return true;
+  }
 }
 
 void led_set_user(uint8_t usb_led) {
